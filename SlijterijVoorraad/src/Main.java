8358b6bf42b65f053edf831354ffda24fd12e1d9
SirThana <martijn.zijl.3000@gmail.com>
2019-02-07 20:42:59 +0100

    a

--- SlijterijVoorraad/src/Main.java
+++ SlijterijVoorraad/src/Main.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("this is a test");
         System.out.println("Welkom bij Slijterij 't Lammetje, geschreven door: <Naam> <Klas> <Studentnr>\n");
 
         testStap2();
